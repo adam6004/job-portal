1d5b61f46004a91210c7738eaae294e8ecb45bc6
adam6004 <chytraadam@gmail.com>
2025-09-19 01:12:16 +0200

    changed User model, added some basic template styles

--- jobApp/models.py
+++ jobApp/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    nickname = models.CharField(max_length=40)
     login = models.CharField(max_length=40)
     password = models.CharField(max_length=40)
     def __str__(self):
-        return self.nickname
+        return self.login
 
 class Job(models.Model):
     title = models.CharField(max_length=50)
