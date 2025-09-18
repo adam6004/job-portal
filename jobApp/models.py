298482f4b67e1b26b5d80e2ff8ebf9de345cbaa3
adam6004 <chytraadam@gmail.com>
2025-09-18 02:40:35 +0200

    added basic job model

--- jobApp/models.py
+++ jobApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateField(null=True)
+    due_date = models.DateField(null=True)
